c62ecec5bbe76623acb9facdf5de31275a8b697c
L-Utsav <lutsav.97@gmail.com>
2019-10-09 10:38:27 +0530

    second commit

---
+++ src/main/java/com/stackroute/movieAppTasks/MovieAppTasksApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.movieAppTasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAppTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieAppTasksApplication.class, args);
+	}
+
+}
